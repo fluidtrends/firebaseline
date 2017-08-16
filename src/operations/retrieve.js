f01a1ccc3660e3d61d79398ca871277cfab2015f
I. Dan Calinescu <idancalinescu@gmail.com>
2017-08-17 00:46:43 +0300

    fixed retrieve

--- src/operations/retrieve.js
+++ src/operations/retrieve.js
@@ -1,27 +1,39 @@
 function retrieve(firebase, args) {
   var ref = firebase.database().ref(args.key)
+  var byId = true
 
   if (args.orderBy) {
+    byId = false
     ref = ref.orderByChild(args.orderBy)
   }
 
   if (args.equalTo) {
+      byId = false
       ref = ref.equalTo(args.equalTo)
   } else {
     if (args.endAt) {
+      byId = false
       ref = ref.endAt(args.endAt)
     }
 
     if (args.limitToLast) {
+      byId = false
       ref = ref.limitToLast(args.limitToLast)
     }
   }
 
-
   return ref.once('value').
           then(snapshot => {
               var data = snapshot.val()
-              data = Object.keys(data).map(_id => Object.assign({}, data[_id], { _id }))
+
+              if (byId) {
+                return data
+              }
+
+              data = Object.keys(data).map(key => {
+                return Object.assign({}, data[key], { _id: key })
+              })
+
               return (data.length === 1 ? data[0] : data)
   })
 }
