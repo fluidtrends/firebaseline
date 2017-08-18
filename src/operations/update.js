93466433596fee66a44c9cf88ef01fbe6391ef8e
I. Dan Calinescu <idancalinescu@gmail.com>
2017-08-19 01:12:15 +0300

    added update functionalityt

--- src/operations/update.js
+++ src/operations/update.js
@@ -1,29 +1,8 @@
 function update(firebase, args) {
     const key = args.key
-    // const [node, index, value] = key.split('/')
     delete args.key
 
-    return firebase.database().ref(key).set(Object.assign({ timestamp: new Date().getTime() }, args))
-
-    // if (!value) {
-    //     // Get the node by id
-    //     return firebase.database().ref('/' + node + (index ? ("/" + index) : "")).once('value').
-    //            then(snapshot => Object.keys(snapshot.val())[0]).
-    //            then(key => {
-    //              console.log(key)
-    //            })
-    // }
-    //
-    // // Get the node by id
-    // return firebase.database().ref('/' + node).orderByChild(index).equalTo(value).once('value').
-    //        then(snapshot => {
-    //          const data = snapshot.val()
-    //          const key = Object.keys(data)[0]
-    //          const values = data[key]
-    //          var updates = {}
-    //          updates["/" + node + "/" + key] = Object.assign({}, values, args)
-    //          return firebase.database().ref().update(updates)
-    //        })
+    return firebase.database().ref().child("/" + key).update(args)
 }
 
 module.exports = update
