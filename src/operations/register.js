097c5ff62ea661c918b8ac7be3e4de583c5ff0f1
I. Dan Calinescu <idancalinescu@gmail.com>
2017-09-12 16:20:20 +0300

    fixed admin register

--- src/operations/register.js
+++ src/operations/register.js
@@ -3,22 +3,27 @@ function register(firebase, args) {
   var creation = Promise.resolve()
 
   if (args.appAuth) {
-    creation = firebase.auth().createUserWithEmailAndPassword(args.email, args.password)
-  } else {
-    create = firebase.auth().createUser({
+    return firebase.auth().createUserWithEmailAndPassword(args.email, args.password).
+            then(user => firebase.database().ref('users/' + user.uid).set({
+              email: args.email,
+              name: args.name,
+              timestamp: new Date().getTime()
+            }))
+  }
+
+
+  return firebase.auth().createUser({
         email: args.email,
         emailVerified: true,
         password: args.password,
         displayName: args.name,
-        disabled: false
-    })
-  }
-
-  return creation.then(user => firebase.database().ref('users/' + user.uid).set({
+        disabled: false }).
+    then(user => firebase.database().ref('users/' + user.uid).set({
       email: args.email,
       name: args.name,
       timestamp: new Date().getTime()
     }))
+
 }
 
 module.exports = register
