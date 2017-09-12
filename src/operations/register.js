4fae0245e9a71532f04abc6c148312cd0bd3817f
I. Dan Calinescu <idancalinescu@gmail.com>
2017-09-12 16:09:59 +0300

    updated register

--- src/operations/register.js
+++ src/operations/register.js
@@ -1,13 +1,20 @@
 function register(firebase, args) {
 
-  return firebase.auth().createUser({
-      email: args.email,
-      emailVerified: true,
-      password: args.password,
-      displayName: args.name,
-      disabled: false
-    }).
-    then(user => firebase.database().ref('users/' + user.uid).set({
+  var creation = Promise.resolve()
+
+  if (args.appAuth) {
+    creation = firebase.auth().createUserWithEmailAndPassword(args.email, args.password)
+  } else {
+    create = firebase.auth().createUser({
+        email: args.email,
+        emailVerified: true,
+        password: args.password,
+        displayName: args.name,
+        disabled: false
+    })
+  }
+
+  return creation.then(user => firebase.database().ref('users/' + user.uid).set({
       email: args.email,
       name: args.name,
       timestamp: new Date().getTime()
