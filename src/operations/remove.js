e0521e7fa808dadc8fcbecf0c9a1bfa652904c64
I. Dan Calinescu <idancalinescu@gmail.com>
2017-09-15 00:27:05 +0300

    added remove fixes

--- src/operations/remove.js
+++ src/operations/remove.js
@@ -1,15 +1,7 @@
 function remove(firebase, args) {
     const key = args.key
 
-    return new Promise((resolve, reject) => {
-      firebase.database().ref().child(args.key).remove((error) => {
-        if (error) {
-          reject(error)
-          return
-        }
-        resolve()
-      })
-    })
+    return firebase.database().ref().child(args.key).remove()
 }
 
 module.exports = remove
