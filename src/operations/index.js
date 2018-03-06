2175fd7b89dd628c8530215bdbec3fe6994d7e54
AndyComan <andy@evanghelic.ro>
2018-03-06 20:55:28 +0200

    Export reset function

--- src/operations/index.js
+++ src/operations/index.js
@@ -4,6 +4,7 @@ const join = require('./join')
 const register = require('./register')
 const unregister = require('./unregister')
 const retrieve = require('./retrieve')
+const reset = require('./reset')
 const login = require('./login')
 const update = require('./update')
 const subscribe = require('./subscribe')
@@ -11,4 +12,4 @@ const unsubscribe = require('./unsubscribe')
 const remove = require('./remove')
 const store = require('./store')
 
-module.exports = { add, create, join, store, register, unregister, retrieve, login, subscribe, unsubscribe, update, remove }
+module.exports = { add, create, join, store, register, unregister, retrieve, reset, login, subscribe, unsubscribe, update, remove }
