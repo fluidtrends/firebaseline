732bddb4e7a124753ac392b4932ea1f60fbe4e22
I. Dan Calinescu <idancalinescu@gmail.com>
2018-05-03 20:50:29 +0300

    retrieve profile when logging in

--- src/operations/login.js
+++ src/operations/login.js
@@ -2,10 +2,12 @@ const retrieve = require('./retrieve')
 
 function login (firebase, args) {
   if (!args.email || !args.password) {
-    throw new Error("Email and password required")
+    throw new Error('Email and password required')
   }
 
   return firebase.auth().signInWithEmailAndPassword(args.email, args.password)
+          .then((user) => user.toJSON())
+          .then((user) => retrieve({ key: `users/${user.uid}` }).then((profile) => ({ user, profile })))
 }
 
 module.exports = login
