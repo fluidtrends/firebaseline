7e951f9a71d8222ee158b02d34b3b7d7bab0402b
I. Dan Calinescu <idancalinescu@gmail.com>
2018-05-03 21:06:46 +0300

    fixed login database issue

--- src/operations/login.js
+++ src/operations/login.js
@@ -7,7 +7,7 @@ function login (firebase, args) {
 
   return firebase.auth().signInWithEmailAndPassword(args.email, args.password)
           .then((user) => user.toJSON())
-          .then((user) => retrieve({ key: `users/${user.uid}` }).then((profile) => Object.assign({}, user, profile)))
+          .then((user) => retrieve(firebase, { key: `users/${user.uid}` }).then((profile) => Object.assign({}, user, profile)))
 }
 
 module.exports = login
