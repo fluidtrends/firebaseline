28a78e32adc305731cd1837459e20fabd2956477
I. Dan Calinescu <idancalinescu@gmail.com>
2018-05-04 09:24:43 +0300

    login after register

--- src/operations/register.js
+++ src/operations/register.js
@@ -1,27 +1,28 @@
-function register(firebase, args) {
+const login = require('./login')
 
+function register (firebase, args) {
   const password = args.password
+  const email = args.email
   delete args.password
 
   var creation = Promise.resolve()
   if (args.appAuth) {
-    return firebase.auth().createUserWithEmailAndPassword(args.email, password).
-            then(user => firebase.database().ref('users/' + user.uid).set(Object.assign({
+    return firebase.auth().createUserWithEmailAndPassword(args.email, password)
+            .then(user => firebase.database().ref('users/' + user.uid).set(Object.assign({
               timestamp: new Date().getTime()
             }, args)))
   }
 
-
   return firebase.auth().createUser({
-        email: args.email,
-        emailVerified: true,
-        password: password,
-        displayName: args.name,
-        disabled: false }).
-    then(user => firebase.database().ref('users/' + user.uid).set(Object.assign({
-      timestamp: new Date().getTime()
-    }, args)))
-
+    email,
+    emailVerified: false,
+    password: password,
+    disabled: false
+  })
+  .then(user => firebase.database().ref('users/' + user.uid).set(Object.assign({
+    timestamp: new Date().getTime()
+  }, args)))
+  .then(() => login(firebase, { email, password }))
 }
 
 module.exports = register
