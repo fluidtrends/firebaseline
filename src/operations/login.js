010c602dd17b28c4c058f420f6bb515cd9df4a7e
I. Dan Calinescu <idancalinescu@gmail.com>
2018-05-04 12:18:06 +0300

    added more secure login

--- src/operations/login.js
+++ src/operations/login.js
@@ -7,7 +7,7 @@ function login (firebase, args) {
 
   return firebase.auth().signInWithEmailAndPassword(args.email, args.password)
           .then((user) => user.toJSON())
-          .then((user) => retrieve(firebase, { key: `users/${user.uid}` }).then((profile) => Object.assign({}, user, profile)))
+          .then((user) => retrieve(firebase, { key: `users/${user.uid}` }).then((profile) => Object.assign({}, profile)))
 }
 
 module.exports = login
