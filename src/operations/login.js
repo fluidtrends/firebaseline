3eea236b74b87c16439b415ffe4b342d5ce6aa27
I. Dan Calinescu <idancalinescu@gmail.com>
2018-05-04 12:21:25 +0300

    added login token info automatically

--- src/operations/login.js
+++ src/operations/login.js
@@ -7,7 +7,8 @@ function login (firebase, args) {
 
   return firebase.auth().signInWithEmailAndPassword(args.email, args.password)
           .then((user) => user.toJSON())
-          .then((user) => retrieve(firebase, { key: `users/${user.uid}` }).then((profile) => Object.assign({}, profile)))
+          .then((user) => retrieve(firebase, { key: `users/${user.uid}` })
+          .then((profile) => Object.assign({}, profile, { token: firebase.auth().currentUser.getToken() })))
 }
 
 module.exports = login
