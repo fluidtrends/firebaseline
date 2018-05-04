45e84fe09958ee676320dc159725539208cca782
I. Dan Calinescu <idancalinescu@gmail.com>
2018-05-04 19:30:06 +0300

    using token id when logging in

--- src/operations/login.js
+++ src/operations/login.js
@@ -8,7 +8,7 @@ function login (firebase, args) {
   return firebase.auth().signInWithEmailAndPassword(args.email, args.password)
           .then((user) => user.toJSON())
           .then((user) => retrieve(firebase, { key: `users/${user.uid}` })
-          .then((profile) => Object.assign({}, profile, { token: `${firebase.auth().currentUser.getToken()}` })))
+          .then((profile) => Object.assign({}, profile, { token: firebase.auth().currentUser.getIdToken() })))
 }
 
 module.exports = login
